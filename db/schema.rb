ec43366d1d6b55be5323e7c06e4f4396bf6b9b0d
Marsel Shiriyazdanov <marsel.shiriyazdanov@flatstack.com>
2016-07-09 21:16:14 +0300

    add comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709181142) do
+ActiveRecord::Schema.define(version: 20160709181431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text",       null: false
+    t.integer  "user_id",    null: false
+    t.integer  "post_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
