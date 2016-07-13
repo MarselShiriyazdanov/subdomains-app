1c2603655ce35c726947602dccacf92260f2c8ea
Marsel Shiriyazdanov <marsel.shiriyazdanov@flatstack.com>
2016-07-13 22:44:15 +0300

    hide sign in link at naked domains

--- spec/features/user/account/cancel_spec.rb
+++ spec/features/user/account/cancel_spec.rb
@@ -10,7 +10,6 @@ feature "Cancel Account" do
   scenario "User cancels account" do
     click_link "Cancel my account"
 
-    expect(page).to have_content("Sign in")
     expect(page).to have_content("Bye! Your account has been successfully cancelled. We hope to see you again soon.")
 
     click_link "Sign in"
--- spec/features/user/sign_out_spec.rb
+++ spec/features/user/sign_out_spec.rb
@@ -4,7 +4,7 @@ feature "Sign Out" do
   include_context "current user signed in"
 
   scenario "User signs out" do
-    visit "/"
+    visit root_url(subdomain: current_user.company.name)
     click_link "Sign out"
 
     expect(page).to have_content("Sign in")
