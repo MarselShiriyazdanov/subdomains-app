e5b53f571711a74fecfbebb55475411a3143d8dc
Marsel Shiriyazdanov <marsel.shiriyazdanov@flatstack.com>
2016-07-14 18:53:00 +0300

    fix env var

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: "_rails_base_session", domain: {
-  production: ENV["cookies_domain"],
+  production: ENV["COOKIES_DOMAIN"],
   development: ".dev.com"
 }.fetch(Rails.env.to_sym, :all)
